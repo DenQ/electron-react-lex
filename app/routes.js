097ef6d4bcd13a04f1900faff7fd54c510c1757c
DenQ <denquick@gmail.com>
2017-09-22 17:33:17 +0300

    Move Counter to folder

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/Home/HomePage';
-import CounterPage from './containers/CounterPage';
+import CounterPage from './containers/Counter/CounterPage';
 
 export default () => (
   <App>
