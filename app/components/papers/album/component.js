a4332c4fce7ed66d5c4d233c896f923cd9bf178c
DenQ <denquick@gmail.com>
2017-10-03 20:53:13 +0300

    Add menu to paper/album component

--- app/components/papers/album/component.js
+++ app/components/papers/album/component.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Paper } from 'material-ui';
+import RaisedButton from 'material-ui/RaisedButton';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 
 const styles = {
   paper: {
@@ -8,6 +12,11 @@ const styles = {
     margin: 10,
     textAlign: 'center',
     display: 'inline-block',
+    backgroundColor: 'silver',
+  },
+  menu: {
+    position: 'relative',
+    top: 118,
   },
   text: {
     color: 'black',
@@ -18,8 +27,31 @@ export default class AlbumPaper extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      open: false,
+    };
     this.handlerOnClick = this.handlerOnClick.bind(this);
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
   }
+
+  handleTouchTap = (event) => {
+    event.stopPropagation();
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
 
   handlerOnClick() {
     const { record, urlManagerActions } = this.props;
@@ -33,6 +65,28 @@ export default class AlbumPaper extends Component {
         <div style={styles.text}>
           {record.name}
         </div>
+
+        <div style={styles.menu}>
+          <RaisedButton
+            onClick={this.handleTouchTap}
+            label="Click me"
+            fullWidth={true}
+          />
+          <Popover
+            open={this.state.open}
+            anchorEl={this.state.anchorEl}
+            anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+            onRequestClose={this.handleRequestClose}
+          >
+            <Menu>
+              <MenuItem primaryText="Run" />
+              <MenuItem primaryText="Edit" />
+              <MenuItem primaryText="Remove" />
+            </Menu>
+          </Popover>
+        </div>
+
       </Paper>
     );
   }
