a3096fbfedfa3cbbf222c4095f0112dbe9217ab5
DenQ <denquick@gmail.com>
2017-10-03 16:34:12 +0300

    Extract main container

--- app/containers/pages/add-album/container.js
+++ app/containers/pages/add-album/container.js
@@ -1,27 +1,4 @@
-// !@flow
-import React from 'react';
+import Container from '../../../libs/container';
 import Component from '../../../components/pages/add-album/component';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import * as urlManagerActions from '../../../actions/url-manager';
-import * as albumsActions from '../../../actions/albums';
 
-function mapStateToProps(state) {
-  return {
-    urlManager: state.urlManager,
-    albums: state.albums,
-    forms: state.form,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    urlManagerActions: bindActionCreators(urlManagerActions, dispatch),
-    albumsActions: bindActionCreators(albumsActions, dispatch),
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Component);
+export default Container(Component);
--- app/containers/pages/run-album/container.js
+++ app/containers/pages/run-album/container.js
@@ -1,23 +1,4 @@
-// @flow
-import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import Container from '../../../libs/container';
 import Component from '../../../components/pages/run-album/component';
-import * as urlManagerActions from '../../../actions/url-manager';
 
-function mapStateToProps(state) {
-  return {
-    urlManager: state.urlManager,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    urlManagerActions: bindActionCreators(urlManagerActions, dispatch),
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Component);
+export default Container(Component);
