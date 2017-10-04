0d5154dbf4d908dc1aa120efa7db17a2bc0b8de4
DenQ <denquick@gmail.com>
2017-10-04 19:20:39 +0300

    Compare last-opened for reducer

--- app/reducers/album.js
+++ app/reducers/album.js
@@ -1,3 +1,5 @@
+import compare from '../utils/compare-last-opened';
+
 const initialState = {
   records: [],
   record: {}
@@ -6,13 +8,13 @@ const initialState = {
 export default function(state = initialState, action) {
   const { type, records, record } = action;
   if (type === 'list') {
+    records.sort(compare);
     return { records };
   }
   if (type === 'add') {
     return state;
   }
   if (type === 'get') {
-    console.log(222, type, records, record);
     return { record, records: [] };
   }
   return state;
