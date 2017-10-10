bc0d634d9cd49fd687737c2e059740a7413c8877
DenQ <denquick@gmail.com>
2017-10-10 20:38:09 +0300

    Split 300 words to 6 albums

--- app/actions/default-album.js
+++ app/actions/default-album.js
@@ -2,8 +2,9 @@ import db from 'lex/db/lex.dexie';
 import { LIST } from 'lex/constants/album';
 import dump from 'lex/db/default-album';
 
-const ALBUM_ALRADY_EXISTING = 'album_alrady_existing'
-const albumName = 'Irregular verbs(big)';
+const ALBUM_ALRADY_EXISTING = 'album_alrady_existing';
+const COUNT_ALBUMS = 6;
+const albumName = 'Irregular verbs';
 const { albums, words } = db;
 
 export function run() {
@@ -15,16 +16,29 @@ export function run() {
         if (results.length > 0) {
           return Promise.reject({code: ALBUM_ALRADY_EXISTING});
         } else {
-          return albums.add({
-            name: albumName,
-            description: 'About 300 words',
-            cretedDT: +new Date,
-            default: 1,
-          });
+          const pull = [];
+          for(let i=0; i<COUNT_ALBUMS; i++) {
+            const request =  albums.add({
+              name: `${albumName} (Part ${i+1})`,
+              description: 'Default album',
+              cretedDT: +new Date,
+              default: 1,
+            });
+            pull.push(request);
+          }
+          return Promise.all(pull);
         }
       })
-      .then((albumId) => {
+      .then((albums) => {
+        const lenthAlbums = albums.length;
+        let counter = 0;
         const queries = dump.map((item) => {
+          const albumId = albums[counter];
+          if (counter <= lenthAlbums) {
+            counter++;
+          } else {
+            counter = 0;
+          }
           const { originalWord, translateWord } = item;
           return words.add({
             createdDT: +new Date,
@@ -33,7 +47,7 @@ export function run() {
             albumId,
           });
         });
-        return Promise.all(queries)
+        return Promise.all(queries);
       })
       .then(() => albums.toArray())
       .then((records) => {
