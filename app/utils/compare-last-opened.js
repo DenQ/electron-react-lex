9ecbd6caa7a51141fa790ef7ab8a94b94f6e6492
DenQ <denquick@gmail.com>
2017-10-10 20:46:52 +0300

    Fix bug with sort albums

--- app/utils/compare-last-opened.js
+++ app/utils/compare-last-opened.js
@@ -1,6 +1,6 @@
 export default function(a, b) {
-  const alast = a.lastOpened;
-  const blast = b.lastOpened;
+  const alast = a.lastOpened || a.id;
+  const blast = b.lastOpened || b.id;
   if (alast < blast) {
     return 1;
   } else if (alast > blast) {
