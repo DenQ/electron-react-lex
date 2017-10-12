2c7b2a2b2b18bab4a42eb6022f9fd142b43e36ff
DenQ <denquick@gmail.com>
2017-10-12 12:14:27 +0300

    Indigo theme done

--- app/themes/dark-indigo.js
+++ app/themes/dark-indigo.js
@@ -1,56 +1,49 @@
 import {
-  indigo300, indigo700, indigo900,
-  pinkA100, pinkA200, pinkA400, pinkA700,
-  pink500, pink700,
-  grey100, grey300, grey400, grey500,
-  cyan500, cyan700,
+  indigo900 as primary1Color,
+  indigo700 as primary2Color,
+  indigo300 as primary3Color,
+
+  pinkA200 as accent1Color,
+  pinkA100 as accent2Color,
+  pinkA400 as accent3Color,
+
+  red800 as errorColor,
+  grey300,
   white, darkBlack, fullBlack
 } from 'material-ui/styles/colors';
 import { fade } from 'material-ui/utils/colorManipulator';
-// import spacing from '../spacing';
 
 const palette = {
-  primary1Color: indigo900,
-  primary2Color: indigo700,
-  primary3Color: indigo300,
-  accent1Color: pinkA200,
-  accent2Color: pinkA100,
-  accent3Color: pinkA400,
+  primary1Color,
+  primary2Color,
+  primary3Color,
+  accent1Color,
+  accent2Color,
+  accent3Color,
   textColor: white,
   alternateTextColor: white,
-  canvasColor: indigo300,
+  canvasColor: primary3Color,
   borderColor: grey300,
   disabledColor: fade(darkBlack, 0.3),
-  pickerHeaderColor: cyan500,
-  clockCircleColor: fade(darkBlack, 0.07),
+  pickerHeaderColor: white,
+  clockCircleColor: fade(darkBlack, 0.7),
   shadowColor: fullBlack,
 };
 
 export default {
-  // spacing: spacing,
   palette,
   fontFamily: 'Roboto, sans-serif',
   raisedButton: {
-    disabledColor: pinkA100,
+    secondaryColor: accent1Color,
+    disabledColor: accent2Color,
     disabledTextColor: fade(white, 0.6),
   },
-  menu: {
-    backgroundColor: palette.canvasColor,
-    containerBackgroundColor: palette.canvasColor,
+  textField: {
+    errorColor: errorColor,
   },
   menuItem: {
-    // dataHeight: 32,
-    // height: 48,
-    hoverColor: fade(palette.textColor, 0.9),
-    // padding: spacing.desktopGutter,
-    selectedTextColor: palette.accent1Color,
-    rightIconDesktopFill: indigo700,
+    hoverColor: fade(primary2Color, 0.6),
   },
-  // menuSubheader: {
-  //   padding: spacing.desktopGutter,
-  //   borderColor: palette.borderColor,
-  //   textColor: palette.primary1Color,
-  // },
   body:{
     backgroundColor: palette.primary2Color,
   }
