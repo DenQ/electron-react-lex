ce738e5f9a9b994309fa0badc5a2575c3c7d323b
DenQ <denquick@gmail.com>
2017-10-13 10:44:21 +0300

    Fix bug with empty album where run testing

--- app/actions/run.js
+++ app/actions/run.js
@@ -5,6 +5,7 @@ import { LIST } from 'lex/constants/run';
 
 const { words } = db;
 const SIZE = 6;
+const ALBUM_IS_EMPTY = true;
 
 export function list(albumId) {
   albumId = Number(albumId);
@@ -13,6 +14,9 @@ export function list(albumId) {
       .where({ albumId })
       .toArray()
       .then((records) => {
+        if (records.length === 0) {
+          return Promise.reject({code: ALBUM_IS_EMPTY});
+        }
         const vector = Boolean(getRandomInt(0, 1));
         const questionIndex = getRandomInt(0, records.length - 1);
         const question = records[questionIndex];
@@ -21,7 +25,7 @@ export function list(albumId) {
           .sort(compareRandom)
           .slice(0, SIZE);
         answers.push(question);
-        for(let i=0;i<2*SIZE;i++) {
+        for(let i=0; i<2*SIZE; i++) {
           answers.sort(compareRandom);
         }
         return dispatch({
@@ -31,6 +35,10 @@ export function list(albumId) {
           vector,
         });
       })
-      .catch(error => console.error(error));
+      .catch((error) => {
+        if (error.code !== ALBUM_IS_EMPTY) {
+          console.error(error)
+        }
+      });
   }
 }
